f1f21881fd9a3ec1f7ebbe939a6368d03ffa067c
BenjaminDG <degeest.benjamin@vdabcampus.be>
2020-09-03 18:22:27 +0200

    voorbeeld 1

---
+++ voorbeeld1/voorbeeld1/Controllers/FiliaalController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace voorbeeld1.Controllers
+{
+    public class FiliaalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View();
+        }
+        public IActionResult Read(int? id)
+        {
+            //lees in de database het filiaal waarvan de id gelijk is aan de waarde van de parameter id
+            return View();
+        }
+    }
+}
