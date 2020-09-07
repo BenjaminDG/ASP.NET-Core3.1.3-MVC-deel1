7ef988782cacec19f8cf1ec738f151f922eadae8
BenjaminDG <degeest.benjamin@vdabcampus.be>
2020-09-07 16:22:14 +0200

    oefenin hfdst 4 even redirect oneven

---
+++ MeerOverControllers/MeerOverControllers/MeerOverControllers/Controllers/WerknemerController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeerOverControllers.Controllers
+{
+    public class WerknemerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return File(@"images\happy.png", "image/png");
+        }
+
+        [NonAction]
+        public string HuidigeDatum()
+        {
+            return DateTime.Now.ToShortDateString();
+        }
+
+        public IActionResult Read(int id)
+        {
+            return View();
+        }
+
+        public IActionResult VerdubbelDeWeddes()
+        {
+            //update in de database
+            //....
+            return Redirect("~/Werknemer/WeddesAangepast");
+        }
+        public IActionResult WeddesAangepast()
+        {
+            return View();
+        }
+
+        public IActionResult Even(int id)
+        {
+            if (id % 2 == 0)
+            {
+                return View();
+            }
+            else
+            {
+                return Redirect("~/werknemer/oneven");
+            }
+
+            
+        }
+        public IActionResult oneven() 
+        {
+            return View();
+        }
+
+    }
+}
---
+++ voorbeeld1/voorbeeld1/Controllers/WebShopController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace voorbeeld1.Controllers
+{
+    public class WebShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Registreer()
+        {
+            return View();
+        }
+        public IActionResult WinkelMandje() 
+        {
+            return View(); 
+        }
+    }
+}
