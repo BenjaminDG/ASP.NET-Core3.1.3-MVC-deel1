28578d4f8772c0a24109c1255698246d178f67fa
BenjaminDG <degeest.benjamin@vdabcampus.be>
2020-09-08 18:09:55 +0200

    hoofdstuk 6 oefening bieren p 49

---
+++ BierenApplication/BierenApplication/Controllers/BierController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BierenApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BierenApplication.Controllers
+{
+    public class BierController : Controller
+    {
+        public IActionResult Index()
+        {
+            var bieren = new List<Bier>();
+            bieren.Add(new Bier { Id = 0, Naam = "Hoegaarden", Alcohol = 4.9F });
+            
+            bieren.Add(new Bier{Id = 15, Naam = "Felix", Alcohol = 7 });
+            bieren.Add(new Bier { Id = 17, Naam = "Roman", Alcohol = 7.5F });
+            return View(bieren);
+        }
+    }
+}
---
+++ BierenApplication/BierenApplication/Models/Bier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BierenApplication.Models
+{
+    public class Bier
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public float Alcohol { get; set; }
+    }
+}
