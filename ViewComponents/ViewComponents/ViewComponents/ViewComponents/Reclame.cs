17b2795eab63c809e541ab618e168fad6d6bba1a
BenjaminDG <degeest.benjamin@vdabcampus.be>
2020-09-10 14:07:02 +0200

    view component parameter

---
+++ ViewComponents/ViewComponents/ViewComponents/ViewComponents/Reclame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace ViewComponents.ViewComponents
+{
+    public class Reclame: ViewComponent
+    {
+        static string[] powerbars = { "mars", "bounty", "snickers", "twix" };
+        public IViewComponentResult Invoke(string tekst, bool html)
+        {   if (html)
+                return new HtmlContentViewComponentResult(new HtmlString(tekst));
+            else return Content(tekst);
+        }
+    }
+}
