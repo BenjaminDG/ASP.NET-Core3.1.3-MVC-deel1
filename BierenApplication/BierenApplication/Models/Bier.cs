2df8f336977caedb2faf2c3276288751299fccbe
BenjaminDG <degeest.benjamin@vdabcampus.be>
2020-09-11 13:54:14 +0200

    oefening pg 69 kleuren opmaak bieren

--- BierenApplication/BierenApplication/Models/Bier.cs
+++ BierenApplication/BierenApplication/Models/Bier.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,10 @@ namespace BierenApplication.Models
 {
     public class Bier
     {
+        [DisplayFormat(DataFormatString ="{0:00}")]
         public int Id { get; set; }
         public string Naam { get; set; }
+        [UIHint("kleuren")]
         public float Alcohol { get; set; }
     }
 }
